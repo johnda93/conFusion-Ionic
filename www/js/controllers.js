5d5d45a7eb2c2610e61da2e4cd14bae8f4a77506
johnda93 <jodcastrori@unal.edu.co>
2016-07-11 21:00:57 -0500

    Implementing the favorites filter and updating the sidebar

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -242,4 +242,16 @@ angular.module('conFusion.controllers', [])
       favoriteFactory.deleteFromFavorites(index);
       $scope.shouldShowDelete = false;
 
-    }}]);
+    }}])
+    .filter('favoriteFilter', function () {
+      return function (dishes, favorites) {
+        var out = [];
+        for (var i = 0; i < favorites.length; i++) {
+          for (var j = 0; j < dishes.length; j++) {
+            if (dishes[j].id === favorites[i].id)
+              out.push(dishes[j]);
+          }
+        }
+        return out;
+
+      }});
