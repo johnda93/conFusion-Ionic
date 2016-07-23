8e2a979fa035ab2a4622a7098750aa6a0be77b53
johnda93 <jodcastrori@unal.edu.co>
2016-07-23 02:17:33 -0500

    Updating AppCtrl controller to use the local storage

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('conFusion.controllers', [])
-  .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  .controller('AppCtrl', function($scope, $ionicModal, $timeout, $localStorage) {
 
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
@@ -11,7 +11,7 @@ angular.module('conFusion.controllers', [])
     //});
 
     // Form data for the login modal
-    $scope.loginData = {};
+    $scope.loginData = $localStorage.getObject('userinfo','{}');
     $scope.reservation = {};
 
     // Create the reserve modal that we will use later
@@ -62,6 +62,7 @@ angular.module('conFusion.controllers', [])
     // Perform the login action when the user submits the login form
     $scope.doLogin = function() {
       console.log('Doing login', $scope.loginData);
+      $localStorage.storeObject('userinfo',$scope.loginData);
 
       // Simulate a login delay. Remove this and replace with your login
       // code if using a login system
